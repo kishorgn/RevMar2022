a3f80ff84f9f2011566a4d6d0c97b5e9d272ac82
kishor <kishor@Kishor-ThinkPad>
2022-03-15 09:28:43 +0530

    Commit - 3 : Added a forloop in Prog2

--- GitProject_01/src/com/rev/app/App_02.java
+++ GitProject_01/src/com/rev/app/App_02.java
@@ -4,6 +4,10 @@ public class App_02 {
 
 	public static void main(String[] args) {
 		System.out.println("Program - 2");
+		for(int c = 0 ; c < 10 ; c ++) {
+			System.out.println(c);
+		}
+		System.out.println("Thank you!!!");
 
 	}
 
