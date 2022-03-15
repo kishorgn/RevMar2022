cda49138a7f5587cf4859a8ced574bf844349762
kishor <kishor@Kishor-ThinkPad>
2022-03-15 09:26:14 +0530

    Commit - 2 : Added line2 in prog1 and new prog2.

--- GitProject_01/src/com/rev/app/App_01.java
+++ GitProject_01/src/com/rev/app/App_01.java
@@ -4,6 +4,7 @@ public class App_01 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!!!");
+		System.out.println("Thank you!!!");
 
 	}
 
---
+++ GitProject_01/src/com/rev/app/App_02.java
@@ -0,0 +1,10 @@
+package com.rev.app;
+
+public class App_02 {
+
+	public static void main(String[] args) {
+		System.out.println("Program - 2");
+
+	}
+
+}
